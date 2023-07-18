6e03682f826e9fea9a6ab1ec928ad6aba774d805
DuongHao999 <duongtuhao999@gmail.com>
2023-07-18 08:45:02 +0700

    Count item, hadle when having multiple promise.

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,16 +2,35 @@ import CourseModel from "../models/Course.js";
 import { multipleMoongoseToObject } from "../util/moongose.js";
 
 //  [GET] /me/store/courses
-export function storeCourses(req, res, next) {
+export async function storeCourses(req, res, next) {
 
-    CourseModel.find({})
-        .then(
-            courses => {
-                // res.json(courses);
-                res.render('me/store-courses', { courses: multipleMoongoseToObject(courses) });
-            }
-        )
+    // Nếu trong function có nhiều promise => thì cần đưa vào promise all để xử lý
+    Promise.all([CourseModel.find({}), CourseModel.countDocuments()])
+        .then(([courses, count]) => {
+            // console.log(courses, count);
+            res.render('me/store-courses', { courses: multipleMoongoseToObject(courses), count: count });
+        })
         .catch(next);
+
+    // Vẫn dùng được 
+    const courses = await CourseModel.find({});
+    const count = await CourseModel.countDocuments();
+    res.render('me/store-courses', { courses: multipleMoongoseToObject(courses), count });
+
+    // CourseModel.countDocuments()
+    //     .then(count => {
+    //         console.log(count);
+    //     })
+    //     .catch(next);
+
+    // CourseModel.find({})
+    //     .then(
+    //         courses => {
+    //             // res.json(courses);
+    //             res.render('me/store-courses', { courses: multipleMoongoseToObject(courses) });
+    //         }
+    //     )
+    //     .catch(next);
 }
 
 //  [GET] /me/trash/courses
